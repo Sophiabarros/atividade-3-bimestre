6634712c69fa57b1788dd74986407b037a651c2d
Sophia Barros 32B <soohbarros1@gmail.com>
2026-08-24 14:48:37 -0300

    eu mudei algo, shhhhh, é segredo.

--- frontend/script.js
+++ frontend/script.js
@@ -9,7 +9,7 @@ async function buscarFilmes() {
             <div>
                 <h2>${filme.titulo}</h2>
                 <p><strong>Gênero:</strong> ${filme.genero}</p>
-                <p><strong>Duração:</strong> ${filme.duracao} minutos</p>
+                <p><strong>Duração:</strong> ${filme.duracao}</p>
                 <p><strong>Classificação indicativa:</strong> ${filme.classificacao > 0 ? filme.classificacao + ' anos' : 'Livre'}</p>
             </div>
         `
